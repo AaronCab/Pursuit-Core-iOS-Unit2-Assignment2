09e4feb6e42e077ca5587556253de3f06ccabc65
AaronCab <Aaron@pursuits-macbook-pro-17.local>
2018-11-21 12:04:18 -0500

    "Final Version"

--- GameOfThrones/GameOfThrones/Controllers/DetailViewController.swift
+++ GameOfThrones/GameOfThrones/Controllers/DetailViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class DetailViewController: UIViewController {
     var episode: GOTEpisode!
+    
+    @IBOutlet weak var episodeImage: UIImageView!
+    
+    @IBOutlet weak var episodeName: UILabel!
+    
+    
+    @IBOutlet weak var seasonNumber: UILabel!
+    
+    @IBOutlet weak var episodeNumber: UILabel!
+    
+    
+    @IBOutlet weak var runTime: UILabel!
+    
+    
+    @IBOutlet weak var airDate: UILabel!
+    
+    
+    @IBOutlet weak var episodeDescription: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateRecipeUI()
     }
     private func updateRecipeUI() {
-//        episodeImage.image = UIImage(named: episode.mediumImageID)
-//        episodeName.text = episode.name
-//        episodeDesctiption.text = episode.description
+        episodeImage.image = UIImage(named: episode.mediumImageID)
+        episodeName.text = episode.name
+        episodeDescription.text = episode.summary
+        runTime.text = "Runtime: \(episode.runtime)"
+        seasonNumber.text = "Season: \(episode.season)"
+        episodeNumber.text = "Episode: \(episode.number)"
+        airDate.text = "AirDate: \(episode.airdate)"
+        
     }
 
 }
